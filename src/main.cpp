e89b13ea0d73e5e2485fc5097b90108f7132728c
tamimurashid <Rashidytamimu@gmail.com>
2025-07-22 12:43:34 +0300

    update

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,26 @@ const char* password = "200120022003";
 
 // ====== DEVICE AUTHENTICATION ===
 
+const char* device_id = "";
+const char* auth_token = "";
 
+// ===== DECLARATION ============
 
+// ===== Sensor Pins =====
+#define DHTPIN      4
+#define DHTTYPE     DHT22
+#define FLAME_PIN   35
+#define SMOKE_PIN   34
+#define BUZZER_PIN  23
+
+// ===== Objects =====
 ChatbotAPI chatbot;
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+DHT dht(DHTPIN, DHTTYPE);
+
+
+//  ====== SETUP  FUNCTION =====
+void setup(){
+  Serial.begin(115200);
+  
+}
